c3e2e31d99ed86300aba883d0e0ee4d8506e2b69
Bilal <bilalzubairi@hotmail.com>
2022-01-14 20:44:11 +0500

    section 12 start

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asaf'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       // only whitelisted property (defined in dto) get passed, rest are stripped
---
+++ src/users/auth.service.ts
@@ -0,0 +1,52 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email already in use');
+    }
+
+    // hash password
+    // 1 -- generate salt
+    const salt = randomBytes(8).toString('hex');
+
+    // 2 -- hash salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // 3 -- join hashed result and salt together
+    const result = `${salt}.${hash.toString('hex')}`;
+
+    // create user and save
+    const user = await this.usersService.create(email, result);
+
+    // return user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    return user;
+  }
+}
