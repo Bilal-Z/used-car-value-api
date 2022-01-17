79ce0c151f7044de4d87d63eaf853f85413e39c6
Bilal <bilalzubairi@hotmail.com>
2022-01-17 18:12:21 +0500

    section 18 start

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -41,5 +41,7 @@ export class ReportsController {
   }
 
   @Get()
-  getEstimate(@Query() query: GetEstimateDto) {}
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
 }
